358c88307c50c43942b40ebd16ac67d5b3f1118b
Ayush-sharma-webdev <sapanailu23@gmail.com>
2025-04-11 19:12:58 +0530

    changed

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const cookieParser = require('cookie-parser');
 const indexRouter = require('./Routes/index.route');
 const userRouter = require('./Routes/user.route');
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join( __dirname , 'public')));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
-app.set("view engine", "ejs");
-app.use(express.static(path.join( __dirname , 'public')));
 
 app.use('/' , indexRouter);
 
