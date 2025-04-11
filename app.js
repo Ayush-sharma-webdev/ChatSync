f0c209c5e0cbed7bffb4492d08bc6f40829788c5
Ayush-sharma-webdev <sapanailu23@gmail.com>
2025-04-11 19:09:43 +0530

    views

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const userRouter = require('./Routes/user.route');
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
-app.set('view engine' , 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static(path.join( __dirname , 'public')));
 
 app.use('/' , indexRouter);
