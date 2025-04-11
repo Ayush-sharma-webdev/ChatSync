fd7fc29a3501a3d0768a9c5a990e9e75f3cb08a6
Ayush-sharma-webdev <sapanailu23@gmail.com>
2025-04-11 19:17:28 +0530

    Add views directory with index.ejs

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const indexRouter = require('./Routes/index.route');
 const userRouter = require('./Routes/user.route');
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join( __dirname , 'public')));
 app.use(cookieParser());
 app.use(express.json());
